41d9473f23d12d5296dc9a70f38472a7b047d5a1
ScottyP666 <Scott_M_G@live.com>
2021-11-08 21:31:05 -0800

    Final product
    Comments added appropriately to explain whats going on.

--- src/com/GuessingGame/GameControl.java
+++ src/com/GuessingGame/GameControl.java
@@ -3,48 +3,79 @@ package com.GuessingGame;
 import java.util.Scanner;
 
 public class GameControl {
-
+	//instantiating a scanner to assign a variable to each of the users guess
 	static Scanner scanner = new Scanner(System.in);
+	//variable holding the value of the users guess per round
 	static int userGuess;
+	
+	//this is to set the index during the game, and as a condition when telling the player to pick higher or lower
+	//this is purely because i don't want to see the phrase "Please pick a higher number"/"Please pick a lower number" after the final guess. it annoys me. The game is over they can't pick a higher or lower number. it makes no sense.
+	private int index;
+
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	//variable that determines an early loop termination condition
+	boolean winner;
+	// The magic variable of which users will be trying to guess
+	private static int randNumber =  (int) Math.floor((Math.random()* 100) + 1);
+	
+	public static int getRandNumber() {
+		return randNumber;
+	}
+
+
+	static int convertedGuess(double userNumber) {
+		return (int) Math.round(userNumber);
+	}
+	
+	
+
+	
+	
+	
+	//method called to set the value of userGuess each round, to compare against the number trying to be guessed
 	static void setGuess() {
 		System.out.println("Pick a number between 1 and 100:");
 		double userNumber = scanner.nextDouble();
+		//validation that the user guessed a number between 1 and 100, even if it has a decimal, then 
+		//its converted to a standard int for comparison against the random number
 		while (userNumber < 1 || userNumber > 100) {
 			System.out.println("Your guess is not between 1 and 100, please try again");
 			System.out.println("Pick a number between 1 and 100:");
 			userNumber = scanner.nextDouble();
 		}
+		//the variable userGuess for this particular instantiation is set to an integer through
+		//the public convertedGuess method
 		userGuess = GameControl.convertedGuess(userNumber);
 	}
 	
-	static int convertedGuess(double userNumber) {
-		return (int) Math.round(userNumber);
-	}
-	
-	int randomlyGeneratedNumber;
-	boolean winner;
-
-	void setRandNum() {
-		this.randomlyGeneratedNumber = (int) Math.round((Math.random()* 100) + 1);
-	}
-	
+	//these strings only print to the console when the user has another guess, otherwise this whole thing is 
+	//skipped all-together
 	void playRound() {
 		GameControl.setGuess();
-		if (userGuess < randomlyGeneratedNumber) {
+		if (userGuess < getRandNumber() && this.getIndex() < 4) {
 			System.out.println("Please pick a higher number :");
-		} else if(userGuess > randomlyGeneratedNumber) {
+		} else if(userGuess > getRandNumber() && this.getIndex() < 4) {
 			System.out.println("Please pick a lower number: ");
-		}else if (userGuess == randomlyGeneratedNumber) {
+		}else if (userGuess == getRandNumber()) {
 			this.winner = true;
 		}
 	}
 	
-	void didYahWinSon() {
+	//the last part of the requirements, where fates are judged and dunce caps are distributed, names after a meme
+	void areYahWinningSon() {
 		if (this.winner) {
 			System.out.println("You Win!");
 		} else if (!this.winner) {
 			System.out.println("You Lose!");
 		}
-		System.out.println("The number to guess was: " + this.randomlyGeneratedNumber);
+		System.out.println("The number to guess was: " + getRandNumber());
 	}
+
+
 }
--- src/com/GuessingGame/GuessingGame.java
+++ src/com/GuessingGame/GuessingGame.java
@@ -4,17 +4,22 @@ package com.GuessingGame;
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		GameControl playAGame = new GameControl();
-		playAGame.setRandNum();
 		
-		for (int i = 0; i < 5; i++) {
+		
+		GameControl playAGame = new GameControl();
+				
+		//too see why the for loop is based on a variable inside GameControl, see the comment above $private int index inside the class GameControl
+		for (playAGame.getIndex() ; playAGame.getIndex() < 5; playAGame.setIndex(playAGame.getIndex() + 1)) {
+			//winner is a boolean that sets an early termination condition through a setter
 			if(playAGame.winner) {
-				i = 9000;
+				playAGame.setIndex(9000);
 			} else if (!playAGame.winner) {
 				playAGame.playRound();
 			}
 		}
+		//method that outputs whether the user was successful or must wear the dunce cap
+		playAGame.areYahWinningSon();
 		
-		playAGame.didYahWinSon();
+		//I've worn the dunce cap many times
 	}
 }
